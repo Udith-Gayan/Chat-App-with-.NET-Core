e6886bdc583c17babd96ecd5e0e86d4594e4328d
Udith Gayan Indrakantha <udith@geveo.com>
2020-07-23 17:50:02 +0530

    Frontend Interface Created.

--- frontend/web-chat-app/src/app/app.component.ts
+++ frontend/web-chat-app/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+  }
   title = 'web-chat-app';
+
+  msgDto: MessageDto = new MessageDto();
+
+  send(): void {
+    if(this.msgDto) {
+      if(this.msgDto.user.length == 0 || this.msgDto.user.length == 0){
+        window.alert("Both fields are required.");
+        return;
+      } else {
+        // do send method
+      }
+    }
+  }
+}
+
+class MessageDto {
+  public user: string;
+  public msgText: string;
 }
--- frontend/web-chat-app/src/app/app.module.ts
+++ frontend/web-chat-app/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule} from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
